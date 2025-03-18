434b18286555ebc15b0a0b5586f04c6d839f3c4b
lazar03 <alfonzlazri@gmail.com>
2025-03-18 17:23:45 +0100

    init front

---
+++ src/app/components/heroSection.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { useState } from 'react'
+import NavBar from './navBar';
+
+export default function HeroSection() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+      }}
+    >
+
+      <div className="hero-overlay"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+            quasi. In deleniti eaque aut repudiandae et a id nisi.
+          </p>
+          <button
+            className="btn btn-primary"
+            onClick={() => alert('Button clicked!')}
+          >
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+
+  );
+}
