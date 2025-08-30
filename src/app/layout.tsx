4d3c9f08e5e338ab7c6a3b416686fd86b1101af0
lazari03 <alfonzlazri@yea.al>
2025-08-30 14:57:10 +0200

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { AuthProvider } from "../context/AuthContext";
+import Script from "next/script";
+import Analytics from "./analytics/Analytics";
 
 export const metadata: Metadata = {
   title: "Portokalle",
@@ -14,8 +16,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="light">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID || process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID || process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}');
+          `}
+        </Script>
+      </head>
       <body className="bg-base-100 min-h-screen">
         <AuthProvider>
+          <Analytics />
           {children}
         </AuthProvider>
       </body>
